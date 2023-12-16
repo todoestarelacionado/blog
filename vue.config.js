c440156319bb6b458899755ce610e14023165079
Todo Esta Relacionado <todoestarelacionadocontodo@gmail.com>
2023-12-16 14:57:46 -0300

    Added public path to vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/blog/' : '/',
   transpileDependencies: true,
   devServer: {
     port: 8080,
