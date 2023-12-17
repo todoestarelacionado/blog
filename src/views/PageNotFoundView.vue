0b17064d4ed3656bdca2b3776028424148004ab4
Todo Esta Relacionado <todoestarelacionadocontodo@gmail.com>
2023-12-17 20:24:03 -0300

    TERT-0002 Added 404 page not found

---
+++ src/views/PageNotFoundView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page-not-found-view">
+    <h1>404 Error</h1>
+    <h2>Página no encontrada</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageNotFoundView'
+};
+</script>
+
+<style lang="scss" scoped>
+.page-not-found-view {
+  height: 100vh;
+}
+</style>
